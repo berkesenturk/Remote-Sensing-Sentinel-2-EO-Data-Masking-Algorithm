3dc29a4ad439555b31797f6b42aee9d88a4468ad
berkesenturk <berkesenturk11@gmail.com>
2021-01-01 19:19:49 +0300

    ndwi index and functions are fixed

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# special_topics_in_remote_sensing
+# Description
+
+## Index Creator
+
+Index Creator is a pilot Python repository for solving common Remote Sensing problems. In this assignment, the creation of NDVI and NDWI indices and their masking from water bodies were studied.
+
+Main aim was creating the NDVI, NDWI indexes and masking the water bodies after. To do so, the theoretical knowledge about Near Infrared bands does not reflect from the water bodies, in order to benefit from it a condition has been defined.
+
+However in practical, it does not work everytime because of the effects of another factors such as atmosphere, pixel values of the NIR values are not zero always. Hence, NIR band of Landsat 8 examined and a threshold value is used rather than zero.
+
+# Installation
+
+```git clone git@github.com:berkesenturk/index_creator.git```
+
+## Usage 
+
+To use the repository it needs to be installed and worked within the same directory.
+
+```python
+import utils
+
+utils.create_ndvi(red_band=red,nir_band=nir)
+utils.water_mask_ndvi(ndvi_band=ndvi,nir_band=nir)
+
+```
+
+## How to contribute?
+
+Please create an issue to resolve the problem in order to quick response.
---
+++ utils.py
@@ -0,0 +1,144 @@
+import matplotlib.pyplot as plt
+from skimage import *
+from skimage import io
+import numpy as np
+from skimage.viewer import ImageViewer
+from tifffile import * 
+import matplotlib.patches as mpatches   
+
+def create_ndwi(swir_band,nir_band):
+    """
+    -----------
+    Definition
+    -----------
+
+    NDWI calculator
+
+    input params
+    ------------
+
+    nir_band : tiff file
+    swir_band : tiff file #7.band swir_2
+
+    output
+    ------
+
+    img : tiff file 
+    """
+    np.seterr(divide='ignore', invalid='ignore')
+    ndwi = (nir_band - swir_band) / (nir_band + swir_band)
+    ndwi= np.nan_to_num(ndwi)
+    return ndwi
+    
+def create_ndvi(nir_band,red_band):
+    """
+    -----------
+    Definition
+    -----------
+
+    NDVI calculator
+
+    input params
+    ------------
+
+    red_band : tiff file
+    nir_band : tiff file
+
+    output
+    ------
+
+    img : tiff file 
+    """
+    np.seterr(divide='ignore', invalid='ignore')
+    ndvi = (nir_band - red_band) / (nir_band + red_band)
+    ndvi= np.nan_to_num(ndvi)
+    return ndvi
+
+def water_mask_ndvi(ndvi_band,nir_band):
+    """
+    -----------
+    Definition
+    -----------
+
+    Mask for water bodies
+
+    input params
+    ------------
+
+    ndvi_band : tiff file
+    nir_band : tiff file
+
+    output
+    ------
+
+    img : tiff file 
+    """
+    ndvi_in_uint = (ndvi_band*255).astype('uint8')
+    for x in range(ndvi_in_uint.shape[0]):
+        for y in range(ndvi_in_uint.shape[1]):
+            if (ndvi_in_uint[x][y][1]!=0 and nir_band[x][y][1]<=100): 
+                ndvi_in_uint[x][y][1]=0
+    
+    img = ndvi_in_uint[:, :, 1]
+    return img
+
+def water_mask_ndwi(ndwi_band,nir_band):
+    """
+    -----------
+    Definition
+    -----------
+
+    Mask for water bodies
+
+    input params
+    ------------
+
+    ndvi_band : tiff file
+    nir_band : tiff file
+
+    output
+    ------
+
+    img : tiff file 
+    """
+    ndwi_in_uint = (ndwi_band*255).astype('uint8')
+    for x in range(ndwi_in_uint.shape[0]):
+        for y in range(ndwi_in_uint.shape[1]):
+            if (ndwi_in_uint[x][y][1]!=0 and nir_band[x][y][1]<=50): 
+                ndwi_in_uint[x][y][1]=0
+    
+    img = ndwi_in_uint[:, :, 1]
+    return img
+
+if __name__=='__main__':
+    red = io.imread('test_images_tiff/B4.tif')
+    nir = io.imread('test_images_tiff/B5.tif')
+    swir = io.imread('test_images_tiff/B7.tif')
+    
+    ndvi=create_ndvi(nir_band=nir,red_band=red)
+
+    # masked image
+    img=water_mask_ndvi(ndvi_band=ndvi,nir_band=nir)
+
+    plt.title("NDVI Map of Landsat 8 Satellite Imagery")
+    plt.xlabel("Y coordinates")
+    plt.ylabel("X coordinates")
+
+    plt.imshow(img,cmap='PRGn',vmin=0,vmax=255)
+    plt.colorbar()
+    plt.savefig('outputs/NDVI.png',format="png")
+    plt.show()
+    
+    #ndwi
+    ndwi=create_ndwi(nir_band=nir,swir_band=swir)
+    # masked image
+    img=water_mask_ndwi(ndwi_band=ndwi,nir_band=nir)
+    plt.title("NDWI Map of Landsat 8 Satellite Imagery")
+    plt.xlabel("Y coordinates")
+    plt.ylabel("X coordinates")
+    
+    plt.imshow(img,cmap='PRGn',vmin=0,vmax=255)
+    plt.colorbar()
+    plt.savefig('outputs/NDWI.png',format="png")
+    plt.show()
+    
